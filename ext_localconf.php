47b591c926d448eda106e518f6b75c158fce27c2
jimsuperfly <jimsuperfly@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-24 18:11:51 +0000

    RE-IMPORT: - from extBase-branch back into TRUNK Resolved some minor features: - pre-selection of authentication-system: typo3, sugar or both - form-validition is partially supported by extBase-validation-classes - created a setEntry()-method that can update the contact records of SugarCRM
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sugarmine/trunk@26908 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+/**
+ * Configure the Plugin to call the
+ * right combination of Controller and Action according to
+ * the user input (default settings, FlexForm, URL etc.)
+ */
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,											// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
+	'SugarMine',												// A unique name of the plugin in UpperCamelCase
+	array(												// An array holding the controller-action-combinations that are accessible 
+		'Start' => 'index,soap,test,auth,login',		// The first controller and its first action will be the default 
+		'Account' => 'index,test,form,valid,save',
+		),
+	array(												// An array of non-cachable controller-action-combinations (they must already be enabled)
+		'Start' => 'index,soap,test,auth,login',
+		'Account' => 'index,test,form,valid,save',
+		)
+);
+
+$subTypes = 'authUserFE,getUserFE';
+
+t3lib_extMgm::addService($_EXTKEY,  'auth' /* sv type */,  'tx_sugarmine_sv1' /* sv key */,
+		array(
+
+			'title' => 'SugarMine-Authentication',
+			'description' => 'Authenticates SugarMine-Users',
+
+			'subtype' => $subTypes,
+
+			'available' => TRUE,
+			'priority' => 100,
+			'quality' => 100,
+
+			'os' => '',
+			'exec' => '',
+
+			'classFile' => t3lib_extMgm::extPath($_EXTKEY).'sv1/class.tx_sugarmine_sv1.php',
+			'className' => 'tx_sugarmine_sv1',
+		)
+	);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,165 @@
+<?php
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+/**
+ * Registers a Plugin to be listed in the Backend. You also have to configure the Dispatcher in ext_localconf.php.
+ */
+Tx_Extbase_Utility_Extension::registerPlugin(
+	$_EXTKEY,// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
+	'SugarMine',				// A unique name of the plugin in UpperCamelCase
+	'SugarMine'	// A title shown in the backend dropdown field
+);
+
+if (TYPO3_MODE === 'BE')	{
+	/**
+	* Registers a Backend Module
+	*/
+	/*Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'web',					// Make module a submodule of 'web'
+		'tx_blogexample_m1',	// Submodule key
+		'',						// Position
+		array(																			// An array holding the controller-action-combinations that are accessible
+			'Blog' => 'index,show,new,create,delete,deleteAll,edit,update,populate',	// The first controller and its first action will be the default
+			'Post' => 'index,show,new,create,delete,edit,update',
+			'Comment' => 'create,delete,deleteAll',
+			),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:blog_example/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xml',
+		)
+	);*/
+
+	/**
+	 * Add labels for context sensitive help (CSH)
+	 */
+	//t3lib_extMgm::addLLrefForTCAdescr('_MOD_web_BlogExampleTxBlogexampleM1', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh.xml');
+}
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'SugarMine');
+
+/*
+
+t3lib_extMgm::allowTableOnStandardPages('tx_blogexample_domain_model_blog');
+$TCA['tx_blogexample_domain_model_blog'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xml:tx_blogexample_domain_model_blog',
+		'label' 			=> 'title',
+		'tstamp' 			=> 'tstamp',
+		'crdate' 			=> 'crdate',
+		'versioningWS' 		=> 2,
+		'versioning_followPages'	=> true,
+		'origUid' 			=> 't3_origuid',
+		'languageField' 	=> 'sys_language_uid',
+		'transOrigPointerField' 	=> 'l18n_parent',
+		'transOrigDiffSourceField' 	=> 'l18n_diffsource',
+		'delete' 			=> 'deleted',
+		'enablecolumns' 	=> array(
+			'disabled' => 'hidden'
+			),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/tca.php',
+		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_blog.gif'
+	)
+);
+
+t3lib_extMgm::allowTableOnStandardPages('tx_blogexample_domain_model_post');
+$TCA['tx_blogexample_domain_model_post'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xml:tx_blogexample_domain_model_post',
+		'label'				=> 'title',
+		'label_alt'			=> 'author',
+		'label_alt_force'	=> TRUE,
+		'tstamp'            => 'tstamp',
+		'crdate'            => 'crdate',
+		'versioningWS' 		=> 2,
+		'versioning_followPages'	=> true,
+		'origUid' 			=> 't3_origuid',
+		'languageField' 	=> 'sys_language_uid',
+		'transOrigPointerField' 	=> 'l18n_parent',
+		'transOrigDiffSourceField' 	=> 'l18n_diffsource',
+		'prependAtCopy' 	=> 'LLL:EXT:lang/locallang_general.xml:LGL.prependAtCopy',
+		'copyAfterDuplFields' 		=> 'sys_language_uid',
+		'useColumnsForDefaultValues' => 'sys_language_uid',
+		'delete'            => 'deleted',
+		'enablecolumns'     => array(
+			'disabled' => 'hidden'
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/tca.php',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_post.gif'
+	)
+);
+
+t3lib_extMgm::allowTableOnStandardPages('tx_blogexample_domain_model_comment');
+$TCA['tx_blogexample_domain_model_comment'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xml:tx_blogexample_domain_model_comment',
+		'label'				=> 'date',
+		'label_alt'			=> 'author',
+		'label_alt_force'	=> TRUE,
+		'tstamp'            => 'tstamp',
+		'crdate'            => 'crdate',
+		'versioningWS' 		=> 2,
+		'versioning_followPages'	=> true,
+		'origUid' 			=> 't3_origuid',
+		'languageField' 	=> 'sys_language_uid',
+		'transOrigPointerField' 	=> 'l18n_parent',
+		'transOrigDiffSourceField' 	=> 'l18n_diffsource',
+		'prependAtCopy' 	=> 'LLL:EXT:lang/locallang_general.xml:LGL.prependAtCopy',
+		'copyAfterDuplFields' 		=> 'sys_language_uid',
+		'useColumnsForDefaultValues' => 'sys_language_uid',
+		'delete'            => 'deleted',
+		'enablecolumns'     => array (
+			'disabled' => 'hidden'
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/tca.php',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_comment.gif'
+	)
+);
+
+t3lib_extMgm::allowTableOnStandardPages('tx_blogexample_domain_model_person');
+$TCA['tx_blogexample_domain_model_person'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xml:tx_blogexample_domain_model_person',
+		'label' 			=> 'lastname',
+		'label_alt'			=> 'firstname',
+		'label_alt_force'	=> TRUE,
+		'tstamp' 			=> 'tstamp',
+		'crdate' 			=> 'crdate',
+		'versioningWS' 		=> 2,
+		'versioning_followPages'	=> true,
+		'origUid' 			=> 't3_origuid',
+		'prependAtCopy' 	=> 'LLL:EXT:lang/locallang_general.xml:LGL.prependAtCopy',
+		'delete' 			=> 'deleted',
+		'enablecolumns' 	=> array(
+			'disabled' => 'hidden'
+			),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/tca.php',
+		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_person.gif'
+	)
+);
+
+t3lib_extMgm::allowTableOnStandardPages('tx_blogexample_domain_model_tag');
+$TCA['tx_blogexample_domain_model_tag'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xml:tx_blogexample_domain_model_tag',
+		'label'				=> 'name',
+		'tstamp'            => 'tstamp',
+		'crdate'            => 'crdate',
+		'delete'            => 'deleted',
+		'enablecolumns'     => array (
+			'disabled' => 'hidden'
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/tca.php',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_blogexample_domain_model_tag.gif'
+	)
+);
+*/
+//$extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
+//$pluginSignature = strtolower($extensionName) . '_pi1';
+/*
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
+*/
+?>
